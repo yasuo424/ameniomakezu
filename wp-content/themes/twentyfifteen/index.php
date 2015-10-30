b1edacfb14c143a8231ce4e7157e09dd1fbdde0f
中田 泰雄 <nakata_yasuo@liginc.co.jp>
2015-10-30 12:18:14 +0900

    3番目の編集をコミット

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -1,20 +1,3 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * e.g., it puts together the home page when no home.php file exists.
- *
- * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
-?>
-
 雨ニモマケズ
 風ニモマケズ
 雪ニモ夏ノ暑サニモマケヌ
@@ -24,3 +7,7 @@
 イツモシヅカニワラッテヰル
 一日ニ玄米四合ト
 味噌ト少シノ野菜ヲタベ
+アラユルコトヲ
+ジブンヲカンジョウニ入レズニ
+ヨクミキキシワカリ
+ソシテワスレズ
